7502d31a358c1c822c05dc61be24e1ceb1f5fd3d
Maaz <themaaz32@gmail.com>
2022-07-21 20:15:51 +0500

    feat: exploring column widget

--- lib/main.dart
+++ lib/main.dart
@@ -25,88 +25,30 @@ class MyFirstScreen extends StatelessWidget {
         title: const Text('First Application'),
       ),
       drawer: const Drawer(),
-      body: Center(
+      body: SizedBox(
+        width: double.infinity,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.end,
           children: [
-            const Text(
-              "Count : 0",
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.w600,
-              ),
+            Container(
+              color: Colors.orange,
+              height: 100,
+              width: 100,
             ),
-            TextButton(
-              onPressed: () {},
-              child: const Text('Increment'),
+            Container(
+              color: Colors.red,
+              height: 100,
+              width: 100,
             ),
-            TextButton(
-              onPressed: () {},
-              child: const Text('Decrement'),
+            Container(
+              color: Colors.yellow,
+              height: 100,
+              width: 100,
             ),
           ],
         ),
       ),
-      // floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {
-          print('testing');
-        },
-      ),
     );
   }
 }
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({Key? key, required this.title}) : super(key: key);
-//
-//   final String title;
-//
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-//
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-//
-//
-//   void _incrementCounter() {
-//     setState(() {
-//       _counter++;
-//     });
-//   }
-//   // void _decrementCounter() {
-//   //   setState(() {
-//   //     _counter--;
-//   //   });
-//   // }
-//   @override
-//   Widget build(BuildContext context) {
-//
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headline4,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ),
-//     );
-//   }
-// }
